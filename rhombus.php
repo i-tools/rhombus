4d1b6a75ed4f7b63c31e8d7abdaf0d7bd0a4ea7b
Alex Krasnov <alex@i-tools.ru>
2019-10-28 15:13:39 +0500

    add rhombus.php

---
+++ rhombus.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+$sizeRhombus = 7;
+$countStars = 1;
+$spaceChar = '0';
+$rhombusCHar = '*';
+
+for ( $i = 1; $i <= round($sizeRhombus/2); $i++ )
+{
+    $countSpace = round($sizeRhombus/2) - $i;
+
+    for ( $j = 0; $j < $countSpace; $j++ )
+    {
+        echo $spaceChar;
+    }
+
+    for ( $j = 0; $j < $countStars; $j++ )
+    {
+        echo $rhombusCHar;
+    }
+
+    for ( $j = 0; $j < $countSpace; $j++ )
+    {
+        echo $spaceChar;
+    }
+
+    echo PHP_EOL;
+
+    $countStars = $countStars + 2;
+}
+
+$countStars = $countStars - 4;
+
+for ( $i = round($sizeRhombus/2); $i > 1; $i-- )
+{
+    $countSpace = round($sizeRhombus/2) - $i + 1;
+
+    for ( $j = 0; $j < $countSpace; $j++ )
+    {
+        echo $spaceChar;;
+    }
+
+    for ( $j = 0; $j < $countStars; $j++ )
+    {
+        echo $rhombusCHar;
+    }
+
+    for ( $j = 0; $j < $countSpace; $j++ )
+    {
+        echo $spaceChar;;
+    }
+
+    $countStars = $countStars - 2;
+
+    echo PHP_EOL;
+}
+
